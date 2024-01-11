51d84b34880b03a2340f44de2ef8c463c3b82c53
Oussema <oussama.chercheri@tek-up.de>
2024-01-12 00:05:28 +0100

    first commit

---
+++ EmployeeMgmnt1/Employees.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace EmployeeMgmnt1
+{
+    partial class Employees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            textBox5 = new TextBox();
+            label8 = new Label();
+            textBox6 = new TextBox();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Teal;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(960, 100);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Andalus", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(367, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(236, 26);
+            label1.TabIndex = 1;
+            label1.Text = "Employee Managment System";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Andalus", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.Teal;
+            label2.Location = new Point(408, 130);
+            label2.Name = "label2";
+            label2.Size = new Size(147, 26);
+            label2.TabIndex = 2;
+            label2.Text = "Manage Emplyees";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.Teal;
+            label3.Location = new Point(12, 200);
+            label3.Name = "label3";
+            label3.Size = new Size(109, 26);
+            label3.TabIndex = 7;
+            label3.Text = "Emplyee name";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(12, 229);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(220, 32);
+            textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.Teal;
+            label4.Location = new Point(12, 264);
+            label4.Name = "label4";
+            label4.Size = new Size(109, 26);
+            label4.TabIndex = 9;
+            label4.Text = "Emplyee name";
+            label4.Click += label4_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(12, 293);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(220, 32);
+            textBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.Teal;
+            label5.Location = new Point(12, 392);
+            label5.Name = "label5";
+            label5.Size = new Size(109, 26);
+            label5.TabIndex = 13;
+            label5.Text = "Emplyee name";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(12, 421);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(220, 32);
+            textBox3.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.Teal;
+            label6.Location = new Point(12, 328);
+            label6.Name = "label6";
+            label6.Size = new Size(109, 26);
+            label6.TabIndex = 11;
+            label6.Text = "Emplyee name";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(12, 357);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(220, 32);
+            textBox4.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = Color.Teal;
+            label7.Location = new Point(12, 531);
+            label7.Name = "label7";
+            label7.Size = new Size(109, 26);
+            label7.TabIndex = 17;
+            label7.Text = "Emplyee name";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.Location = new Point(12, 560);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(220, 32);
+            textBox5.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.ForeColor = Color.Teal;
+            label8.Location = new Point(12, 456);
+            label8.Name = "label8";
+            label8.Size = new Size(109, 26);
+            label8.TabIndex = 15;
+            label8.Text = "Emplyee name";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox6.Location = new Point(12, 485);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(220, 32);
+            textBox6.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Teal;
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(0, 602);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(960, 11);
+            panel2.TabIndex = 19;
+            // 
+            // Employees
+            // 
+            AutoScaleDimensions = new SizeF(9F, 26F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(960, 613);
+            Controls.Add(panel2);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label8);
+            Controls.Add(textBox6);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Font = new Font("Andalus", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Employees";
+            Text = "Employees";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label7;
+        private TextBox textBox5;
+        private Label label8;
+        private TextBox textBox6;
+        private Panel panel2;
+    }
+}
